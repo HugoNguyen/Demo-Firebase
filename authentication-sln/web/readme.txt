c9b6cb380a4f0862086be9567519449958f381b3
Nguyen Quoc Tuan <tuan.nguyen@vn.devinition.com>
2020-02-15 12:24:20 +0700

    Squashed commit of the following:
    commit 569b09324e05b621c5eb6851ab243f293b1f4716
    Author: Nguyen Quoc Tuan <tuan.nguyen@vn.devinition.com>
    Date:   Sat Feb 15 12:17:46 2020 +0700
    
        Add firebase SDK
    
    commit 89ed928f8a6b52d291e7adc576b5629b7adaa22a
    Author: Nguyen Quoc Tuan <tuan.nguyen@vn.devinition.com>
    Date:   Sat Feb 15 11:22:39 2020 +0700
    
        add demo-auth-web-app
    
    commit e217a61e9be3976edfa20bd6e2b1c662a744b0e8
    Author: Nguyen Quoc Tuan <tuan.nguyen@vn.devinition.com>
    Date:   Sat Feb 15 11:15:46 2020 +0700
    
        add firebase project
    
    commit 59e9426103ba109a450a7c6145bdbf5f9f11bb19
    Author: Nguyen Quoc Tuan <tuan.nguyen@vn.devinition.com>
    Date:   Sat Feb 15 11:09:22 2020 +0700
    
        intied project

---
+++ authentication-sln/web/readme.txt
@@ -0,0 +1,6 @@
+#Link
+https://firebase.google.com/docs/auth/web/firebaseui
+
+#Main point
+FirebaseUI
+Firebase SDKs v7.8.2
---
+++ misc/demo-auth-web-app.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.8.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.8.2/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCdwwG2ElQ4p29rWx9egvAi22l-Ryg-CHc",
+    authDomain: "fir-firebase-62c88.firebaseapp.com",
+    databaseURL: "https://fir-firebase-62c88.firebaseio.com",
+    projectId: "fir-firebase-62c88",
+    storageBucket: "fir-firebase-62c88.appspot.com",
+    messagingSenderId: "792961507489",
+    appId: "1:792961507489:web:f5a8d72d13dca897a290e0",
+    measurementId: "G-28L8Q9ZWYB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
